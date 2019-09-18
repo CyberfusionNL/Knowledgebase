e1bd1fd0f6956756a8bb98c934b46d340c9e731d
Yvan E. Watchman <ywatchman@cyberfusion.nl>
2019-09-18 17:02:06 +0200

    margin on sidebar...

--- resources/views/partials/articles/sidebar.blade.php
+++ resources/views/partials/articles/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="col-4">
     <ul>
-        <li>
+        <li class="mb-3">
             <h5>LEARN THE BASICS <i style="font-size: .6em" class="fa fa-angle-double-down align-self-center"></i></h5>
             <ul class="purple-bullet">
                 <li><a href="#">Release-384: Users can list flows and set Varnish settings <i style="font-size: .6em" class="fa fa-angle-double-down align-self-center"></i></a></li>
@@ -8,10 +8,10 @@
                 <li><a href="#">Release 312: Billing portal SSO URL endpoint and general improvements</a></li>
             </ul>
         </li>
-        <li>
+        <li class="mb-3">
             <h5>START COLLABORATING <i style="font-size: .6em" class="fa fa-angle-double-right align-self-center"></i></h5>
         </li>
-        <li>
+        <li class="mb-3">
             <h5>MORE FEATURES <i style="font-size: .6em" class="fa fa-angle-double-right align-self-center"></i></h5>
         </li>
     </ul>
