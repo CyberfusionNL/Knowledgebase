bb4d1522c1f1dd7571d07bf2b8a9e1ab039e4eae
Yvan E. Watchman <ywatchman@cyberfusion.nl>
2019-09-19 10:20:29 +0200

    Create user command

---
+++ app/Console/Commands/User/CreateCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands\User;
+
+use App\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class CreateCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cf:user:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create knowledgebase user';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $user = new User;
+        $user->name = $this->ask('Username');
+        $user->email = $this->ask('E-Mail Address');
+        $password = $this->secret('Password');
+        if(strlen($password) > 8) {
+            $user->password = Hash::make($password);
+            unset($password);
+        } else {
+            $this->error('Password too short, aborting...');
+            return;
+        }
+        if($this->confirm('Are you sure to create this user?')) {
+            $user->save();
+            $this->alert("User '$user->name' created!");
+        } else {
+            $this->alert('User has not been created.');
+        }
+    }
+}
