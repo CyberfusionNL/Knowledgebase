658611726e8d908649ee64ea618f798491dcf877
Yvan E. Watchman <ywatchman@cyberfusion.nl>
2019-09-21 18:28:37 +0200

    Fix layout master

--- resources/views/articles/search.blade.php
+++ resources/views/articles/search.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.master')
-@include('partials.article.header')
+
+@section('header')
+    @include('partials.articles.header')
+@endsection
 
 @section('content')
     <!-- header -->
