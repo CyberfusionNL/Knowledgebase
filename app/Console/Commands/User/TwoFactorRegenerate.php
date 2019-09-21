e532895f3c52740de29848e6a1062cd5e37df09a
Yvan E. Watchman <ywatchman@cyberfusion.nl>
2019-09-21 08:15:34 +0200

    Correct description of command cf:user:2fa

--- app/Console/Commands/User/TwoFactorRegenerate.php
+++ app/Console/Commands/User/TwoFactorRegenerate.php
@@ -19,7 +19,7 @@ class TwoFactorRegenerate extends Command
      *
      * @var string
      */
-    protected $description = 'Create user for knowledgebase';
+    protected $description = 'Regenerate Twofactor code for user';
 
     /**
      * Create a new command instance.
