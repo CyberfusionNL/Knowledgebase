2ac66a948611ba2c583ca68602998863d9d76d1a
Yvan E. Watchman <ywatchman@cyberfusion.nl>
2019-09-23 07:39:53 +0200

    Fix favicon path

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Cyberfusion Knowledgebase | {{ $page ?? 'Dashboard' }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" type="image/png" href="img/favicon.png" />
+    <link rel="shortcut icon" type="image/png" href="/img/favicon.png" />
     <link rel="stylesheet" type="text/css" href="/css/style.css?v=new">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css">
 </head>
